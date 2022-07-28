0b7f9ca3de4ccf33bdb801a27a05b85a756992d6
anmoltiwari0225 <anmoltiwari0225@gmail.com>
2022-07-29 02:54:56 +0530

    Initial commit WIP

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,65 +1,26 @@
-import { object, string, TypeOf } from "zod";
+import { object, string, TypeOf } from "zod"
 
-/**
- * @openapi
- * components:
- *  schemas:
- *    CreateUserInput:
- *      type: object
- *      required:
- *        - email
- *        - name
- *        - password
- *        - passwordConfirmation
- *      properties:
- *        email:
- *          type: string
- *          default: jane.doe@example.com
- *        name:
- *          type: string
- *          default: Jane Doe
- *        password:
- *          type: string
- *          default: stringPassword123
- *        passwordConfirmation:
- *          type: string
- *          default: stringPassword123
- *    CreateUserResponse:
- *      type: object
- *      properties:
- *        email:
- *          type: string
- *        name:
- *          type: string
- *        _id:
- *          type: string
- *        createdAt:
- *          type: string
- *        updatedAt:
- *          type: string
- */
-
-export const createUserSchema = object({
-  body: object({
-    name: string({
-      required_error: "Name is required",
+export const createUserSchema =  object({
+    body: object({
+        name: string({
+            required_error: 'Name is required',
+        }),
+        password: string({
+            required_error: 'Password is required',
+        }).min(6, "Password is too short - should be atleast 6 characters"),
+        passwordConfirmation: string({
+            required_error: 'Confirmation Password is required',
+        }),
+        email: string({
+            required_error: 'Email is required',
+        }).email('Add a valid email'),
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: "Passwords do not match",
+        path: ['passwordConfirmation'],
     }),
-    password: string({
-      required_error: "Name is required",
-    }).min(6, "Password too short - should be 6 chars minimum"),
-    passwordConfirmation: string({
-      required_error: "passwordConfirmation is required",
-    }),
-    email: string({
-      required_error: "Email is required",
-    }).email("Not a valid email"),
-  }).refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords do not match",
-    path: ["passwordConfirmation"],
-  }),
 });
 
 export type CreateUserInput = Omit<
-  TypeOf<typeof createUserSchema>,
-  "body.passwordConfirmation"
+    TypeOf<typeof createUserSchema>,
+    "body.passwordConfirmation"
 >;
